5b215e7d602d4d3dd920603644e5aff245919fc6
Clara Villa <clara.villa@convertdigital.com.au>
2026-03-09 08:29:55 +1000

    text extract stack

---
+++ services/stack/TextExtractServiceStack.ts
@@ -0,0 +1,36 @@
+import { App, CfnOutput, Duration, Stack, StackProps } from "aws-cdk-lib";
+import { HttpApi, HttpMethod } from "aws-cdk-lib/aws-apigatewayv2";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import path from "path";
+
+export default class TextExtactServiceStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+
+    const lambda = new NodejsFunction(this, "textExtractServiceHandler", {
+      functionName: "text-extract-service-handler",
+      runtime: Runtime.NODEJS_22_X,
+      timeout: Duration.seconds(10),
+      entry: path.join(__dirname, "../src/textExtractService.ts"),
+    });
+
+
+    const apiIntegration = new HttpLambdaIntegration("apiIntegration", lambda);
+
+    const apiTextExtract = new HttpApi(this, "textExtractApi");
+
+   apiTextExtract.addRoutes({
+      path: "/",
+      methods: [HttpMethod.POST],
+      integration: apiIntegration,
+    });
+
+
+    new CfnOutput(this, "apiURL", {
+      value: apiTextExtract.url as string,
+    });
+  }
+}
